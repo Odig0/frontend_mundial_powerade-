3de3dbd1869b1723c5634508628ac677ad5ee906
Diego Guzman <diegoguzman366@gmail.com>
2026-05-05 14:43:33 -0400

    se agraga html para las analitcas de google

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,6 +60,23 @@ export default function RootLayout({
   return (
     <html lang="es" className="bg-background" suppressHydrationWarning>
       <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm" strategy="beforeInteractive">
+          {`
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),
+                  dl=l!='dataLayer' ? '&l='+l : '';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-THW4SR2G');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+
         {/* Google Publisher Tag (GPT) */}
         <Script
           src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"
@@ -77,6 +94,18 @@ export default function RootLayout({
       </head>
 
       <body className="font-sans antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-THW4SR2G"
+            height="0" 
+            width="0" 
+            style={{display:'none',visibility:'hidden'}}
+          >
+          </iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <div className="min-h-screen">
           {children}
         </div>
