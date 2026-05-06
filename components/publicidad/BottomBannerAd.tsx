18d4f53847e0cb88e657ff59b1a2ce5155afef48
bricorped <brigittcorporativoed@gmail.com>
2026-05-06 12:16:52 -0400

    feat: agregar banner en el footer final

---
+++ components/publicidad/BottomBannerAd.tsx
@@ -0,0 +1,16 @@
+import { AD_UNITS } from '@/lib/ads'
+import AdSlot from './AdSlot'
+
+export default function BottomBannerAd() {
+  return (
+    <div className="hidden md:flex w-full justify-center py-8 bg-background border-t border-border/10 mt-8">
+      <div className="hidden md:block w-full max-w-[1110px]">
+        <AdSlot 
+          config={AD_UNITS.HOME_BOTTOM} 
+          className="w-full"
+          minHeight="100px"
+        />
+      </div>
+    </div>
+  )
+}
